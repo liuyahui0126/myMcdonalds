57991e124a0509b989133832fd4f7517d6b99db6
yaya <liuyahui_0126@126.com>
2018-02-02 18:38:54 +0800

    firstDay

---
+++ projects/index/index.js
@@ -0,0 +1,110 @@
+// projects/my/my.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    listImgUrl: ["/images/index_wallet.png", "/images/index_tag.png", "/images/index_smile.png", "/images/index_clown.png","/images/index_gift.png"],
+    iconUrl: ["/images/mc-holding.png", "/images/scanCode.png", "/images/my.png"],
+    isChange: [true,false,false]
+  },
+  menu:function(){
+    if (this.data.iconUrl[0] == "/images/mc-holding.png"){
+    }else{
+      wx.redirectTo({
+        url: '../index/index'
+      })
+      this.setData({
+        iconUrl: ["/images/mc-holding.png", "/images/scanCode.png", "/images/my.png"],
+        isChange: [true, false, false]
+      })
+    }
+  },
+  scanCode:function(){
+    if (this.data.iconUrl[0] == "/images/scanCode-holding.png"){
+
+    }else{
+      wx.redirectTo({
+        url: '../scanCode/scanCode'
+      })
+      this.setData({
+        iconUrl: ["/images/mc.png", "/images/scanCode-holding.png", "/images/my.png"],
+        isChange: [false, true, false]
+      })
+    }
+  },
+  my:function(){
+    if (this.data.iconUrl[0] == "/images/my-holding.png") {
+
+    } else {
+      wx.redirectTo({
+        url: '../scanCode/scanCode'
+      })
+      this.setData({
+        iconUrl: ["/images/mc.png", "/images/scanCode.png", "/images/my-holding.png"],
+        isChange: [false, false, true]
+      })
+    }
+  },
+  myScore:function(){
+    wx.navigateTo({
+      url: '../scoreLogin/scoreLogin',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ projects/register/register.js
@@ -0,0 +1,87 @@
+// projects/register/register.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  },
+  register: function () {
+    if(this.data.key == this.data.password && this.data.phone){
+      wx.redirectTo({
+        url: '../scanCode/scanCode',
+      })
+    }
+  },
+  phoneNum: function (e) {
+    this.setData({
+      phone: e.detail.value
+    })
+  },
+  keyWord: function (e) {
+    this.setData({
+      key: e.detail.value
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.request({
+      url: "https://www.easy-mock.com/mock/5a743aefc3b9e4478002bb13/keyWord",
+      success:(data)=>{
+        this.setData({
+          password: data.data.data.key
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
