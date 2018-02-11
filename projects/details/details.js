97bfbeab26ef5f1125fa1749086be4385ec12684
yaya <liuyahui_0126@126.com>
2018-02-11 18:32:16 +0800

    lastUpdate

--- projects/details/details.js
+++ projects/details/details.js
@@ -1,4 +1,4 @@
-// projects/scoreStore/scoreStore.js
+// projects/details/details.js
 Page({
 
   /**
@@ -6,73 +6,70 @@ Page({
    */
   data: {
     imgUrls: ['/images/turnPage1.png', '/images/turnPage2.png', '/images/turnPage3.png'],
-    index: 0,
-  },
-  bindchange: function (e) {
-    this.setData({
-      index: e.detail.current
-    })
+    info: ['9元购买菠萝派（1个）+香芋派（1个）', '11元购买菠萝派（1个）+薯条（中 ）（1份）','12元购买菠萝派（1个）+麦旋风奥利奥口味(1个)'],
+    index:0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options.index);
+    this.setData({
+      index: options.index
+    })
   },
-
+  charge:function(){
+    wx.showModal({
+      content: '对不起，您的积分不足',
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
-  details: function (e) {
-    wx.navigateTo({
-      url: `../details/details?index=${this.data.index}`,
-    })
-
-  },
-
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
